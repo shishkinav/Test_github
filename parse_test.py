30d666ec193edd94f5ab91109f627aa2615d2f4d
AnatolyShishkin <shishkinanatoli@gmail.com>
2019-07-31 21:05:50 +0400

    парсинг категорий, подкатегорий и ссылок

--- parse_test.py
+++ parse_test.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+import re, string
 
 # base_url = 'https://mysbor.ru/spravochnik/'
 
@@ -18,16 +18,31 @@ with open(base_html) as html_doc:
 
 content = soup.find('div', {'class': 'content-container'}).div.div.div.div.div
 category_m = content.find_all('div', {'class': 'title'}) # распарсили места с названиями разделов
+# pattern=r'[\n\s]+(.+)[/n/s]+'
+pattern='\w+'
+lst_main_category = []
 for cat in category_m:
-    print(cat.text) # получили на выходе только названия главных категорий
+    tmp = re.findall(pattern, cat.text)
+    lst = ' '
+    lst_main_category.append(lst.join(tmp))
+# получили на выходе только названия главных категорий
 category_s = content.find_all('div', {'class': 'block'}) # распарсили места с названиями разделов
-print(len(category_m))
-print(len(category_s))
-print(
-    len(
-        [cat.find('span') for cat in category_s]
-    )
-)
-# for cat in category_s:
-#     print(cat.a.get('href') + ' - ' + cat.a.get('title')) # ссылку на подкатегорию и название подкатегории
-    # print(cat.a)
+
+cat_s = {}
+count = 1
+tmp_lst = []
+# проходим по каждой из двадцати главных категорий и в словарь сохраняем по каждой список href + название подкатегории
+for cat in category_s:
+    for _ in cat.find_all('a'):
+        tmp_lst.append([_.get('href'), _.get('title')])
+    cat_s.update({count: tmp_lst})
+    tmp_lst = []
+    count += 1
+    # print([[i.get('href'), i.get('title')] for i in cat.find_all('a')]) # ссылку на подкатегорию и название подкатегории
+for key, value in cat_s.items():
+    print(key, value)
+final_dict = {}
+for _ in lst_main_category:
+    final_dict.update({_: cat_s[lst_main_category.index(_) + 1]})
+# for key, value in final_dict.items():
+#     print(key, value)
