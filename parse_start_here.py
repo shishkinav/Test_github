0dff167b67697d1ac535ca2da0cbb1582df1644c
AnatolyShishkin <shishkinanatoli@gmail.com>
2019-08-02 13:15:01 +0400

    Исправлены баги некорректной выгрузки, переписан код для парсинга предвыбора пользователя по разделу и подкатегории

--- parse_start_here.py
+++ parse_start_here.py
@@ -5,7 +5,7 @@ from parse_organization import (
     Organization
 )
 
-relative_path_file_csv = 'data/20190801_organizations.csv'
+
 
 fieldnames = [
     'main_category',
@@ -19,33 +19,57 @@ fieldnames = [
     'email',
     'link_logo'
 ]
-data = []
+
 # приступаем к массовому парсингу ресурса
 spravochnik = ParseMainSpravochnik()
-for main_category, lst_second_category in spravochnik.dict_category.items():
-    # работаем по очереди с каждым разделом
-    for name_second_category, url_link in lst_second_category:
-        # работаем по очереди с каждым подразделом
-        category = ListOrganizationParser(url_link)
-        for name_company, link_company in category.lst_organisations:
-            tmp_lst = [main_category, name_second_category]
-            company = Organization(url_link, name_company)
-            company.get_param()
-            tmp_lst += [
-                company.name_company,
-                company.url_link,
-                company.adress,
-                company.adress,
-                company.boss,
-                company.site,
-                company.email,
-                company.link_logo
-            ]
-            data.append(dict(zip(fieldnames, tmp_lst)))
+# словарь разделов
+cat_main = {number: key for number, key in enumerate(spravochnik.dict_category.keys(), start=1)}
+for k, v in cat_main.items():
+    print(f'[{k}] - {v}')
+choice_1 = int(input('\nВведите номер раздела:\n'))
+name_main_category = cat_main.get(choice_1)
 
+# словарь подкатегорий выбранного раздела для выбора пользователем
+for number, data in enumerate(spravochnik.dict_category.values()):
+    if number == choice_1:
+        cat_second = {
+            number: key for number, key in enumerate(data, start=1)
+        }
+for k, v in cat_second.items():
+    print(f'[{k}] - {v[0]}')
+choice_2 = int(input('\nВведите номер подраздела:\n'))
+# забираем данные для старта парсинга подраздела
+for k, v in cat_second.items():
+    if k == choice_2:
+        name_second_category, link_second_cat = v
+
+# три переменные образованы для парсинга
+print(name_main_category, name_second_category, link_second_cat)
+
+# работаем по очереди с каждым подразделом
+data = []
+relative_path_file_csv = f'data/20190802_{choice_1}_{choice_2}_organizations.csv'
+category = ListOrganizationParser(link_second_cat)
+for name_company, link_company in category.lst_organisations:
+    tmp_lst = [name_main_category, name_second_category]
+    company = Organization(link_company, name_company)
+    company.get_param()
+    tmp_lst += [
+        company.name_company,
+        company.url_link,
+        company.adress,
+        company.adress,
+        company.boss,
+        company.site,
+        company.email,
+        company.link_logo
+    ]
+    data.append(dict(zip(fieldnames, tmp_lst)))
+    print(f'Данные {company.name_company} получены.')
 # записываем данные в csv файл
 csv_dict_writer(
     relative_path_file_csv,
     fieldnames,
     data
 )
+print(f'Файл {relative_path_file_csv} сохранён.')
